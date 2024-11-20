ca7b1e28521bc9c08e4fde554f062eec33664ed5
Ana Heloisa <heloisaangelo.17@gmail.com>
2024-11-20 16:54:24 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function fazerAposta(){
+    let assignedNumber = parseInt(prompt('Aposte um número de 1 a 100.'))
+    let randomNumber = Math.floor(Math.random()* 100) + 1;
+
+    if(assignedNumber > 1 || assignedNumber < 100){
+        if(assignedNumber == randomNumber){
+            alert(`Você venceu! O número sorteado foi ${randomNumber}`)
+        }else{
+            alert(`Você perdeu! O número sorteado foi ${randomNumber}`)
+        }
+    } else {
+        alert('lala')
+    }
+
+
+}
+fazerAposta()
